8c843c1a5d6d03cd741c4fab8419d69f0944a339
Hilko Bengen <bengen@debian.org>
2014-07-06 21:34:47 +0200

    Do not use DTB for armel
    Gbp-Pq: Name 0003-Do-not-use-DTB-for-armel.patch

--- lib/guestfs-internal.h
+++ lib/guestfs-internal.h
@@ -128,6 +128,9 @@
 /* Machine types. */
 #ifdef __arm__
 #define MACHINE_TYPE "virt"
+#ifdef __ARM_PCS_VFP /* do not use DTB for armel */
+#define DTB_WILDCARD "vexpress*a15-tc1.dtb"
+#endif
 #endif
 #ifdef __aarch64__
 #define MACHINE_TYPE "virt"
