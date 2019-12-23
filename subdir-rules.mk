7ebc54bf01089cde2448c42e5ad36479f6c6a128
Hilko Bengen <bengen@debian.org>
2019-12-23 12:46:43 +0100

    Properly handle autogenerated *config.ml files in builddir
    Gbp-Pq: Name 0016-Properly-handle-autogenerated-config.ml-files-in-bui.patch

--- subdir-rules.mk
+++ subdir-rules.mk
@@ -83,8 +83,12 @@ guestfs_am_v_jar_0 = @echo "  JAR     " $@;
 	$(guestfs_am_v_ocamlcmi)$(OCAMLFIND) ocamlc $(OCAMLFLAGS) $(OCAMLPACKAGES) -c $< -o $@
 %.cmo: %.ml
 	$(guestfs_am_v_ocamlc)$(OCAMLFIND) ocamlc $(OCAMLFLAGS) $(OCAMLPACKAGES) -c $< -o $@
+%.cmo: $(builddir)/%.ml
+	$(guestfs_am_v_ocamlc)$(OCAMLFIND) ocamlc $(OCAMLFLAGS) $(OCAMLPACKAGES) -c $< -o $@
 if HAVE_OCAMLOPT
 %.cmx: %.ml
+	$(guestfs_am_v_ocamlopt)$(OCAMLFIND) ocamlopt $(OCAMLFLAGS) $(OCAMLPACKAGES) -c $< -o $@
+%.cmx: $(builddir)/%.ml
 	$(guestfs_am_v_ocamlopt)$(OCAMLFIND) ocamlopt $(OCAMLFLAGS) $(OCAMLPACKAGES) -c $< -o $@
 endif
 
